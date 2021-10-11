22f8e3743c321fa77347d12cd6e48f40316c523b
Otsile <omogotsi2@debswana.bw>
2021-10-11 02:21:28 +0200

    Corrected code for calculating volume of a sphere file foo.R

--- st2195_assignment_4/foo.R
+++ st2195_assignment_4/foo.R
@@ -1,17 +1,30 @@
 # Radius
 r <- 2
 # Function to compute the volume of a sphere with radius r
-volume <- function(r, rho) {
-  3/4*pi*r^2
+# Corrected volume formula
+volume <- function(r) {
+ return(4/3*pi*r^3)
+ 
 }
-# Function to compute the volumes of the spheres with radius r, r^2 and
-r^3
-volume_vector <- function(r) {
-  r <- 22
-  for (r in 2:4){
-    volume(r)
+
+volume(r)
+
+# Function to compute the volumes of the spheres with radius r, r^2 and r^3
+
+
+volume_vector <- function(r) 
+  {
+  
+ volumes = c()
+  for (i in 1:3){
+    volumes = c(volumes, volume(r^i))
   }
+ return(volumes)
 }
+
+
+
 # Run volume_vector(r) and print the volumes of the spheres with radius
-r, r^2 and r^3
-volume_vector(r)
+
+volume_vector(3)
+
